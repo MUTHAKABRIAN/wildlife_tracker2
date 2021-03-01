3b64de23b4bee832e4d22f6da4cbb0ee5e46e0f4
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-03-01 21:30:34 +0300

    add test for Animal model

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,7 +1,13 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AnimalTest {
-
+    @Test
+    public void animalInstantiatesCorrectly(){
+        Animal firstAnimal= new Animal("panthera padus","animal","young",1);
+        assertEquals(true, firstAnimal instanceof Animal);
+    }
 }
